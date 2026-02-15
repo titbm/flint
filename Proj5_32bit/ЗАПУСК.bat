95d3c59d22b5f4b67fbb3ec456c25aabfe2c684d
titbmm <titbmm@gmail.com>
2026-02-15 22:02:39 +0300

    FLINT+ v2.61: оригинал (16-bit) + 32-bit порт (DOS/4GW) + документация

---
+++ Proj5_32bit/ЗАПУСК.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ========================================
+echo   Запуск FLINT+ v2.61 (32-bit) в DOSBox-X
+echo ========================================
+echo.
+"C:\Program Files\DOSBox-X\dosbox-x.exe" -conf "%~dp0dosbox-x.conf"
---
+++ ЗАПУСК.bat
@@ -0,0 +1,6 @@
+@echo off
+echo ==========================================
+echo   Запуск FLINT+ v2.61 (оригинал, 16-bit)
+echo ==========================================
+echo.
+"C:\Program Files\DOSBox-X\dosbox-x.exe" -conf "%~dp0dosbox-x.conf"
